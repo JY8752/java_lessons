592ab18a8de628a35b01d3f2f30ba81461b2a62d
JY8752 <j.y.8752@gmail.com>
2020-03-08 22:43:59 +0900

    interfaceの学習

--- src/AbstractItem.java
+++ src/AbstractItem.java
@@ -1,4 +1,24 @@
 
-public class AbstractItem {
+public abstract class AbstractItem {
+	protected String name;
+	
+	public AbstractItem(String name) {
+		this.name = name;
+	}
+	//抽象メソッド。子クラスで実装。
+	public abstract void print(String parentPath);
 
+}
+
+//FiliItemクラス
+public class FileItem extends AbstractItem {
+	
+	public FileItem(String name) {
+		super(name);
+	}
+	
+	@Override
+	public void print(String parentPath) {
+		System.out.println(parentPath + File.separator + name);
+	}
 }
