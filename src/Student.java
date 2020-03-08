58c48ab6584158457172a416fa34916daf09b028
JY8752 <j.y.8752@gmail.com>
2020-03-08 13:46:58 +0900

    コンストラクタを作成

--- src/Student.java
+++ src/Student.java
@@ -4,6 +4,16 @@ public class Student {
 	String name;
 	int score;
 	static final int MAX_SCORE = 100;
+	
+	//名前とスコアを渡すコンストラクタ
+	Student (String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+	//名前だけを渡すコンストラクタ
+	Student (String name) {
+		this(name, 0);
+	}
 	//メソッドの宣言
 	void printScore() {
 		System.out.println(name + "さんは" + MAX_SCORE + "点満点中" + score + "点です。");
@@ -12,14 +22,14 @@ public class Student {
 //サンプルクラスでインスタンスを作り実行
 class StudentSample {
 	public static void main(String... args) {
-		Student murata = new Student();
-		murata.name = "村田";
+		Student murata = new Student("村田");
+//		murata.name = "村田";
 		murata.score = 80;
 		murata.printScore();
 		
-		Student okada = new Student();
-		okada.name = "岡田";
-		okada.score = 70;
+		Student okada = new Student("岡田", 70);
+//		okada.name = "岡田";
+//		okada.score = 70;
 		okada.printScore();
 	}
 }
